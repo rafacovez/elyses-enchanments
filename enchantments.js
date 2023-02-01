8cdcc1dfd6a204b80fc1e55a5690dd1a9426f3b7
rafacovez <adanescollante@gmail.com>
2023-02-01 09:49:26 -0400

    coded removeItemAtBottom function

--- enchantments.js
+++ enchantments.js
@@ -85,7 +85,8 @@ export function insertItemAtBottom(cards, newCard) {
  * @returns {number[]} the cards without the removed card
  */
 export function removeItemAtBottom(cards) {
-  throw new Error('Implement the removeItemAtBottom function');
+  cards.shift();
+  return cards;
 }
 
 /**
